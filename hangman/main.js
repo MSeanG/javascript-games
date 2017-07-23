32677289f9d8e79d7868a5e758cdeba220b1b358
Sean <sgilmore@michael-sean.com>
2017-07-22 20:30:31 -0400

    Added favicon, basic HTML5 plus some tags for testing js and a word array in main.js

---
+++ hangman/main.js
@@ -0,0 +1,19 @@
+// $(function(){
+    // 
+// });
+/* An array of words */
+var wordlist = [
+    'alphabet',
+    'chapter',
+    'character',
+    'grammar',
+    'letter',
+    'paragraph',
+    'punctuation',
+    'spelling',
+    'sentence',
+    'word'
+];
+/*
+var el = $('#randomWord');
+*/
