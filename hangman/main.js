0c24c55879f9454c12def32eb3d65ae7d7b46693
Sean <sgilmore@michael-sean.com>
2017-07-22 20:46:55 -0400

    Added var to grab a random word from the wordlist and an array and loop to hide the random word with dashes

--- hangman/main.js
+++ hangman/main.js
@@ -14,6 +14,23 @@ var wordlist = [
     'sentence',
     'word'
 ];
-/*
-var el = $('#randomWord');
+/* test for wordlist array
+var el = $('#random-word');
+el.html(wordlist);
+*/
+
+/* Grab a random word from the wordlist array */
+var randomWord = wordlist[Math.floor(Math.random()*wordlist.length)];
+/* test for randomWord from wordlist
+var el = $('#random-word');
+el.html(randomWord);
+*/
+
+/* Set up an answer array and loop through the letters of the random word and hide them with a dash */
+var answerArray = [];
+for(var i = 0; i < randomWord.length; i++);
+    answerArray[i] = '- ';
+/* test for hidden randomWord with correct amount of dashes for the randomWord length
+var el = $('#random-word');
+el.html(answerArray);
 */
